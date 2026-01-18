ec2c99ffd7e25c9dd062b6c5b8396ffd0a90bdbd
Tomdieu <ivantomdio@gmail.com>
2026-01-18 22:53:29 +0100

    Add: backend/jobs/urls.py

---
+++ backend/jobs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import JobOfferViewSet, JobApplicationViewSet
+
+router = DefaultRouter()
+router.register(r'offers', JobOfferViewSet)
+router.register(r'applications', JobApplicationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

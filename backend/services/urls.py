046e57afc451aa046301a912cf907868450ee250
Tomdieu <ivantomdio@gmail.com>
2026-01-18 22:53:30 +0100

    Add: backend/services/urls.py

---
+++ backend/services/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ServiceCategoryViewSet, ServiceViewSet, ServiceRequestViewSet
+
+router = DefaultRouter()
+router.register(r'categories', ServiceCategoryViewSet)
+router.register(r'list', ServiceViewSet)
+router.register(r'requests', ServiceRequestViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

b7aadddfe25776146cc04e4d90f990efb03b268d
Tomdieu <ivantomdio@gmail.com>
2026-01-24 16:32:07 +0100

    Update: backend/services/models.py

--- backend/services/models.py
+++ backend/services/models.py
@@ -18,6 +18,7 @@ class Service(models.Model):
     description = models.TextField()
     base_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     image = models.ImageField(upload_to='services/', blank=True, null=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_services', null=True, blank=True)
 
     def __str__(self):
         return self.name

0f9c8eaeeef023ef4230eba40283149e75478451
Tomdieu <ivantomdio@gmail.com>
2026-01-24 16:32:07 +0100

    Update: backend/users/urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, ProfileViewSet, CandidateProfileViewSet, CompanyProfileViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'profiles', ProfileViewSet)
+router.register(r'candidate-profiles', CandidateProfileViewSet)
+router.register(r'company-profiles', CompanyProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

15414b1674e1c0100011757731a2ce2b369a81c8
Tomdieu <ivantomdio@gmail.com>
2026-01-24 16:32:07 +0100

    Update: backend/jobs/urls.py

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import JobOfferViewSet, JobApplicationViewSet
 
 router = DefaultRouter()
-router.register(r'offers', JobOfferViewSet)
+router.register(r'offers', JobOfferViewSet, basename='joboffer')
 router.register(r'applications', JobApplicationViewSet)
 
 urlpatterns = [

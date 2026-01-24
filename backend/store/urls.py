7950de5284e167c93dc9a6ff70c841ca9982b815
Tomdieu <ivantomdio@gmail.com>
2026-01-24 16:32:07 +0100

    Update: backend/store/urls.py

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CartViewSet, OrderViewSet, SubscriptionPlanViewSet
+from .views import ProductViewSet, CartViewSet, OrderViewSet, SubscriptionPlanViewSet, UserSubscriptionViewSet, OrderItemViewSet, CartItemViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'plans', SubscriptionPlanViewSet)
+router.register(r'subscriptions', UserSubscriptionViewSet)
 router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'cart-items', CartItemViewSet)
 router.register(r'orders', OrderViewSet)
+router.register(r'order-items', OrderItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

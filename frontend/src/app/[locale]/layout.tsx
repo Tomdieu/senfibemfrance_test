f3fe36fd56bd3660c58d6df5df1c3c961aab1a4b
Tomdieu <ivantomdio@gmail.com>
2026-01-24 16:32:07 +0100

    Update: frontend/src/app/[locale]/layout.tsx

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,29 +1,40 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ReactElement } from 'react'
-import { I18nProviderClient } from '@/locales/client'
-import { SessionProvider } from 'next-auth/react'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ReactNode } from "react";
+import { I18nProviderClient } from "@/locales/client";
+import { SessionProvider } from "next-auth/react";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'SEN FIBEM FRANCE - Services Professionnels & Recrutement',
-  description: 'Plateforme de mise en relation professionnelle, recrutement et services aux entreprises - France & Sénégal',
-}
+  title: "SEN FIBEM FRANCE - Services Professionnels & Recrutement",
+  description:
+    "Plateforme de mise en relation professionnelle, recrutement et services aux entreprises - France & Sénégal",
+};
 
-export default async function RootLayout({ params, children }: { params: Promise<{ locale: string }>, children: ReactElement }) {
-  const { locale } = await params
+export default async function RootLayout({
+  params,
+  children,
+}: {
+  params: Promise<{ locale: string }>;
+  children: ReactNode;
+}) {
+  const { locale } = await params;
   return (
     <html lang={locale}>
-
-      <body className={inter.className} suppressContentEditableWarning suppressHydrationWarning>
+      <body
+        className={inter.className}
+        suppressContentEditableWarning
+        suppressHydrationWarning
+      >
         <SessionProvider>
           <I18nProviderClient locale={locale}>
-            {children}
+            <ReactQueryProvider>{children}</ReactQueryProvider>
           </I18nProviderClient>
         </SessionProvider>
       </body>
     </html>
-  )
+  );
 }

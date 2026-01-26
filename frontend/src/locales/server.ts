f66b12a655fbdf7837abfdd81a1515df5179e04f
Tomdieu <ivantomdio@gmail.com>
2026-01-26 23:17:17 +0100

    feat: Update src/locales/server.ts

--- frontend/src/locales/server.ts
+++ frontend/src/locales/server.ts
@@ -1,7 +1,17 @@
 // locales/server.ts
 import { createI18nServer } from 'next-international/server'
- 
+
 export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
   en: () => import('./en'),
-  fr: () => import('./fr')
+  fr: () => import('./fr'),
+  es: () => import('./es'),
+  de: () => import('./de'),
+  it: () => import('./it'),
+  pt: () => import('./pt'),
+  nl: () => import('./nl'),
+  pl: () => import('./pl'),
+  ru: () => import('./ru'),
+  zh: () => import('./zh'),
+  ar: () => import('./ar'),
+  wo: () => import('./wo')
 })

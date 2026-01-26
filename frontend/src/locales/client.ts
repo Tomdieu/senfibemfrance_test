065d01ca1a40320c690984bb67b6540b5152ab7e
Tomdieu <ivantomdio@gmail.com>
2026-01-26 23:16:41 +0100

    feat: Update src/locales/client.ts

--- frontend/src/locales/client.ts
+++ frontend/src/locales/client.ts
@@ -1,9 +1,19 @@
 // locales/client.ts
 "use client"
 import { createI18nClient } from 'next-international/client'
- 
-export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
+
+export const { useI18n, useScopedI18n, I18nProviderClient,useChangeLocale,useCurrentLocale } = createI18nClient({
   en: () => import('./en'),
-  fr: () => import('./fr')
+  fr: () => import('./fr'),
+  es: () => import('./es'),
+  de: () => import('./de'),
+  it: () => import('./it'),
+  pt: () => import('./pt'),
+  nl: () => import('./nl'),
+  pl: () => import('./pl'),
+  ru: () => import('./ru'),
+  zh: () => import('./zh'),
+  ar: () => import('./ar'),
+  wo: () => import('./wo')
 })
  

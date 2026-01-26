9870bd76775eb1a3f42805e873e9274779d83d83
Tomdieu <ivantomdio@gmail.com>
2026-01-26 23:14:32 +0100

    feat: Update src/app/[locale]/layout.tsx

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 }) {
   const { locale } = await params;
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning suppressContentEditableWarning>
       <body
         className={inter.className}
         suppressContentEditableWarning
